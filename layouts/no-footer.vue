1d683f20ca8c7c0e8852a3c2555a120ac4d281a6
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-12-20 18:13:22 +0300

    push the original project

---
+++ layouts/no-footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Nav />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import Nav from '~/components/global/Nav';
+
+export default {
+  components: {
+    Nav,
+  },
+};
+</script>
