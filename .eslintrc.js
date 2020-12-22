b50ce58e18f4b5db4f3e3ce52ca00d255f8de1bf
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-12-22 19:33:24 +0300

    [infra] add additional eslinting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,21 @@ module.exports = {
     browser: true,
     node: true
   },
+  extends: [
+    '@nuxtjs',
+    'eslint:recommended',
+    'google'
+  ],
   parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
     parser: 'babel-eslint'
   },
   // TODO: should fix properly
   // ! Just followed https://chestozo.medium.com/fixing-eslint-no-invalid-this-error-for-fat-arrow-class-methods-a56908ca8bb6
-  plugins: ['babel'],
-  extends: [
-    'eslint:recommended',
-    'google',
-    '@nuxtjs'
+  plugins: [
+    'babel',
+    'import'
   ],
   rules: {
     // Vue
@@ -23,24 +28,16 @@ module.exports = {
     }],
     'vue/no-v-html': ['error'],
     // JavaScript
-    // 'comma-dangle': ['error', 'never'],
-    semi: ['error', 'always'],
-    'require-jsdoc': 'off', // TODO: should turn on and fix accordingly
+    'comma-dangle': ['error', 'never'],
+    'no-console': ['warn'],
+    indent: ['error', 2],
+    'quote-props': ['error', 'as-needed'],
     'max-len': ['warn',
       {
         code: 130
       } // 130 on GitHub, 80 on npmjs.org for README.md code blocks
     ],
-    'no-negated-condition': 'warn',
-    'spaced-comment': ['error', 'always',
-      {
-        exceptions: ['/']
-      }
-    ],
-    // 'quote-props': ['error', 'as-needed'],
-    indent: ['error', 2],
     'arrow-parens': ['error', 'as-needed'],
-    'object-curly-spacing': ['error', 'always'],
     'space-before-function-paren': [
       'error',
       {
@@ -48,8 +45,16 @@ module.exports = {
         named: 'never'
       }
     ],
-    'no-console': ['warn'],
-
+    'no-negated-condition': 'warn',
+    'spaced-comment': ['error', 'always',
+      {
+        exceptions: ['/']
+      }
+    ],
+    'object-curly-spacing': ['error', 'always'],
+    semi: ['error', 'always'],
+    'require-jsdoc': 'off', // TODO: should turn on and fix accordingly
+    'import/exports-last': 'error',
     // TODO: should fix properly
     // ! Just followed https://chestozo.medium.com/fixing-eslint-no-invalid-this-error-for-fat-arrow-class-methods-a56908ca8bb6
     'no-invalid-this': 0,
