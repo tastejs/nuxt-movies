f9b7b00ba6ac15a77e41c5f134407b6dd763a865
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-12-23 19:27:19 +0300

    [fix] fix eslinting errors

--- api/swr.js
+++ api/swr.js
@@ -3,9 +3,12 @@
  * @param {import('http').ServerResponse} res
  * @param {(err?: any) => void} next
  */
-export default (req, res, next) => {
-  if (req.method !== 'GET') { return next() }
 
-  res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
-  next()
-}
+export default (req, res, next) => {
+  if (req.method !== 'GET') {
+    return next();
+  }
+
+  res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
+  next();
+};
